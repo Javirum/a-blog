86101ee48384fdc867fbd36c7a0b657f83ece0a8
Javier <j.romero@whisbi.com>
2020-07-27 19:02:09 +0200

    Initial config with about page

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const About = () => (
+  <Layout>
+    <div>
+      <h2>About Albert</h2>
+      <p>I'm baby pabst stumptown butcher deserunt, duis raw denim poke flexitarian mollit freegan asymmetrical. Chambray palo santo fixie banh mi, incididunt aliquip readymade deep v raw denim distillery man bun marfa sriracha. Tempor kale chips seitan occaecat godard eiusmod. Butcher mollit banh mi incididunt etsy art party non in mustache actually fingerstache vinyl VHS. Hella williamsburg kombucha farm-to-table. Thundercats kombucha jianbing cronut DIY cliche single-origin coffee do hammock ullamco in organic microdosing 90's. Paleo schlitz direct trade officia VHS laboris aliquip hot chicken poke cold-pressed heirloom portland aesthetic put a bird on it.
+
+Freegan brooklyn pinterest hexagon, aliqua truffaut eiusmod cupidatat readymade tofu officia live-edge tousled air plant. Iceland pitchfork echo park glossier ipsum neutra culpa lyft celiac kombucha sint. Aliquip seitan est, chambray try-hard taiyaki semiotics esse truffaut austin brooklyn. Polaroid yuccie williamsburg glossier, flannel elit culpa anim pop-up mumblecore PBR&B reprehenderit banh mi hashtag shoreditch. Artisan kitsch XOXO woke, raw denim lomo selfies bicycle rights try-hard skateboard thundercats veniam cupidatat cray. Post-ironic synth excepteur iPhone.</p>
+    </div>
+  </Layout>
+)
+
+export default About;

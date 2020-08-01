1f24488ffa4bfaeb15000c783283fc1bf88154a6
javirum <romeropaolini@gmail.com>
2020-08-01 17:44:46 +0200

    Create basic blog post layout

---
+++ src/components/postLayout.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Layout from "../components/layout";
+
+class postLayout extends Component {
+  render() {
+    return (
+      <Layout>
+        <div>
+          <h2>Blog Post Layout</h2>
+        </div>
+      </Layout>
+    );
+  }
+}
+
+export default postLayout;
